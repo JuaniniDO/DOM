65bf3d63555441c805627c4887f8ad3641be7cd1
JuanDomínguez <juandominortega92@gmail.com>
2022-10-25 20:33:48 +0200

    Initial atributes

--- js/app.js
+++ js/app.js
@@ -1,46 +1,55 @@
-//Seleccionar elementos desde js
-let nav = document.querySelector("nav");
-//console.log(nav);
+let box1 = document.querySelector("div");
+console.log(box1);
+let box2 = box1.nextElementSibling;
+console.log(box2);
+let box3 = box2.nextElementSibling;
+console.log(box3);
 
-//Seleccionar elemento con la clase
-let active = document.querySelector(".active");
-//console.log(active);
+box1.classList.add("dark");
+box1.classList.remove("dark");
+box2.classList.add("dark");
+console.log(box3.classList.contains("dark"));
 
-//Seleccionar elementos con una misma etiqueta
-let headersAndParagraphs = document.querySelectorAll("p, h1");
-//console.log(headersAndParagraphs[0].innerText); //se accede al elemento con los corchetes y a su contenido con .innerText elimando todas las etiquetas a su paso
+box3.classList.toggle("dark");
+box3.classList.toggle("dark");
+box3.classList.toggle("dark");
 
-//Acciendo a elementos hijos mediante .children[numHijo]
-//console.log(nav.children[0].children[0].children[0]);
+//Atributo dataset
+let number = box3.dataset.boxNumber;
+console.log(number);
+console.log(box3.dataset.month);
+/* document.body.firstElementChild.innerHTML = "<h1>Wow!</h1>"
 
-///console.log(nav.firstElementChild.firstElementChild.firstElementChild.parentNode.nextElementSibling.firstElementChild.firstChild.parentNode);
+document.querySelector("section") //para seleccionar un elemento
+console.log(document.querySelector("section").innerHTML); //cambia html interno
+console.log(document.querySelector("section").outerHTML); //cambia html desde el propio elemento
+document.querySelector("section").outerHTML = "<h1>Wow!</h1>"  */
 
-//console.log(headersAndParagraphs[2]);
-//console.log(headersAndParagraphs[0].nextElementSibling.nextElementSibling.nextElementSibling);
+//Modify content
+/* document.body.insertAdjacentHTML("afterbegin", "<nav>navigation</nav>");
+document.querySelector("nav").insertAdjacentHTML("beforebegin", "<h1>Main Header</h1>");
+document.querySelector("nav").insertAdjacentHTML("beforeend", '<a href="https://www.google.com/">Ir a google</a>');
+document.querySelector("nav").insertAdjacentHTML("afterend", "<p> This is a paragraph </p>"); */
 
-let main = document.querySelector("#first");
-//console.log(main);
-
-//console.log(nav.firstElementChild.lastElementChild.firstElementChild.firstChild);
-//console.log(nav.firstElementChild.childElementCount);
-//console.log(nav.firstElementChild.children.length);
-//console.log(nav.firstElementChild.childNodes);
-//console.log(nav.firstElementChild.firstChild.nodeValue);
-//Nombre del Nodo en mayusculas
-//console.log(nav.firstElementChild.nodeName);
-
-console.log(nav.firstElementChild.firstChild.nextSibling.nextSibling.previousSibling);
-
-/* //Elemento padre
-document.body.parentNode
-//Total de elementos hijos
-document.body.children.length
-document.body.childElementCount
-//Primer elemento hijo
-document.body.children[0]
-//Primer y ultimo nodos hijos
-document.body.firstElementChild
-document.body.lastElementChild
-//Siguiente y anterior hermano
-document.body.nextElementSibling
-document.body.previousElementSibling */
+//Insertar, borrar y reemplazar nodos en el arbol
+let header = document.createElement("h1");
+header.append("Hello World!");
+document.body.append(header);
+header.prepend("!");
+document.querySelector("h1").firstChild.before("!!");
+let paragraph = document.createElement("p");
+paragraph.textContent = "Writting a paragraph";
+document.body.append(paragraph);
+document.querySelector("h1").replaceWith(paragraph);
+//paragraph.remove()
+//document.body.remove()
+paragraph.style.textAlign = "center";
+paragraph.style.textTransform = "capitalize";
+paragraph.style.backgroundColor = "black";
+paragraph.style.color = "white";
+paragraph.style.padding = "2em";
+paragraph.setAttribute("class","light");
+paragraph.setAttribute("class","main");
+console.log(paragraph.getAttribute("class"));
+paragraph.classList.add("light");
+console.log(paragraph.style.cssText);
